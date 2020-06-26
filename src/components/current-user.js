2fad9a8c9e1f43b85ce446578f4ce13a2463c335
luismangubat <joseluismangubat@gmail.com>
2020-06-25 23:34:20 -0400

    Update current-user.js

---
+++ src/components/current-user.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CurrentUser = () => {
+    return (
+        <div className='current-user-container'>
+            <div></div>
+            <span></span>
+            <div></div>
+        </div>
+    )
+}
